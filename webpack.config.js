b22292f976bdb17798e367a96e387cd6e51eec44
Alejandro Daza Truque <adazat@pslcol.com.co>
2018-05-29 15:57:05 -0500

    Se realiza la estructura basica del proyecto agregando las dependecias de angular y typescritp, ademas se realizar la webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ScriptExtPlugin = require('script-ext-html-webpack-plugin');
+const { AngularCompilerPlugin } = require('@ngtools/webpack');
+
+module.exports = function () {
+   return {
+    module : {
+        rules: [
+            { test: /\.css$/, use: 'css-loader' },
+      { test: /\.ts$/, use: 'ts-loader' }
+        ]
+    },
+    resolve:{
+        extensions: ['.ts', '.js']
+    },
+       entry: './src/main.ts',
+       output: {
+           path: __dirname + '/dist',
+           filename: 'app.js'
+       },
+        mode: 'development',
+       plugins: [
+           new CopyWebpackPlugin([
+               { from: 'src/assets', to: 'assets'}
+           ]), 
+           new HtmlWebpackPlugin({
+               template: __dirname + '/src/index.html',
+               output: __dirname + '/dist',
+               inject: 'head'
+           }), 
+            new ScriptExtPlugin({
+                defaultAttribute: 'defer'
+            }),
+            new AngularCompilerPlugin({
+                tsConfigPath: './tsconfig.json',
+                entryModule: './src/app/app.module#AppModule',
+                sourceMap: true
+             })
+       ]
+   };
+}
