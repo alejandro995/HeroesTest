e425e88d78e136a6c1a8912afc27edcb995f433a
Alejandro Daza Truque <adazat@pslcol.com.co>
2018-05-30 09:51:29 -0500

    Se crear un nuevo bundle para solucionar la carga de estilos

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const  HtmlWebpackPlugin  =  require('html-webpack-plugin');
-const  CopyWebpackPlugin  =  require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ScriptExtPlugin = require('script-ext-html-webpack-plugin');
 const { AngularCompilerPlugin } = require('@ngtools/webpack');
 
-module.exports =  function  ()  {
-    return  {
+module.exports = function () {
+    return {
         module: {
             rules: [
                 { test: /\.css$/, use: 'css-loader' },
@@ -14,26 +14,41 @@ module.exports =  function  ()  {
                         'angular2-template-loader',
                         'ts-loader'
                     ]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 8192
+                            }
+                        }
+
+                    ]
                 }
             ]
         },
         resolve: {
             extensions: ['.ts', '.js']
         },
-        entry:  './src/main.ts',
-        output:  {
-            path:  __dirname +  '/dist',
-            filename:  'app.js'
+        entry: {
+            app: './src/main.ts',
+            styles: './src/assets/styles/main.css'
+        },
+        output: {
+            path: __dirname + '/dist',
+            filename: '[name].js'
         },
         mode: 'development',
-        plugins:  [
-            new  CopyWebpackPlugin([
-                {  from:  'src/assets',  to:  'assets' }
+        plugins: [
+            new CopyWebpackPlugin([
+                { from: 'src/assets', to: 'assets' }
             ]),
-            new  HtmlWebpackPlugin({
-                template:  __dirname +  '/src/index.html',
-                output:  __dirname +  '/dist',
-                inject:  'head'
+            new HtmlWebpackPlugin({
+                template: __dirname + '/src/index.html',
+                output: __dirname + '/dist',
+                inject: 'head'
             }),
             new ScriptExtPlugin({
                 defaultAttribute: 'defer'
