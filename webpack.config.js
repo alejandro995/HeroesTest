2375a501dd643fe8d245e7fa4364e108086edb67
Alejandro Daza Truque <adazat@pslcol.com.co>
2018-05-30 09:15:36 -0500

    "Se agrega el loader de angular2-template para obtener los estilos para los componentes de angular"

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const  HtmlWebpackPlugin  =  require('html-webpack-plugin');
+const  CopyWebpackPlugin  =  require('copy-webpack-plugin');
 const ScriptExtPlugin = require('script-ext-html-webpack-plugin');
 const { AngularCompilerPlugin } = require('@ngtools/webpack');
 
-module.exports = function () {
-   return {
-    module : {
-        rules: [
-            { test: /\.css$/, use: 'css-loader' },
-      { test: /\.ts$/, use: 'ts-loader' }
-        ]
-    },
-    resolve:{
-        extensions: ['.ts', '.js']
-    },
-       entry: './src/main.ts',
-       output: {
-           path: __dirname + '/dist',
-           filename: 'app.js'
-       },
+module.exports =  function  ()  {
+    return  {
+        module: {
+            rules: [
+                { test: /\.css$/, use: 'css-loader' },
+                {
+                    test: /\.ts$/,
+                    loaders: [
+                        'angular2-template-loader',
+                        'ts-loader'
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        entry:  './src/main.ts',
+        output:  {
+            path:  __dirname +  '/dist',
+            filename:  'app.js'
+        },
         mode: 'development',
-       plugins: [
-           new CopyWebpackPlugin([
-               { from: 'src/assets', to: 'assets'}
-           ]), 
-           new HtmlWebpackPlugin({
-               template: __dirname + '/src/index.html',
-               output: __dirname + '/dist',
-               inject: 'head'
-           }), 
+        plugins:  [
+            new  CopyWebpackPlugin([
+                {  from:  'src/assets',  to:  'assets' }
+            ]),
+            new  HtmlWebpackPlugin({
+                template:  __dirname +  '/src/index.html',
+                output:  __dirname +  '/dist',
+                inject:  'head'
+            }),
             new ScriptExtPlugin({
                 defaultAttribute: 'defer'
             }),
@@ -36,7 +42,7 @@ module.exports = function () {
                 tsConfigPath: './tsconfig.json',
                 entryModule: './src/app/app.module#AppModule',
                 sourceMap: true
-             })
-       ]
-   };
+            })
+        ]
+    };
 }
